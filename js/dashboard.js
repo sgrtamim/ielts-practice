5d80ab62ea9c50e1b8e85f1f9e56bf534ebcdfe3
sgrtamim <sgrtamimme@gmail.com>
2026-07-11 17:34:59 +0600

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -24,11 +24,27 @@ Time: ${test.time} minutes
 
 </div>
 
+<div class="timeBox">
+
+<label>Time:</label>
+
+<select id="time-${test.id}">
+
+    <option value="15">15 Minutes</option>
+
+    <option value="20">20 Minutes</option>
+
+    <option value="25">25 Minutes</option>
+
+    <option value="30" selected>30 Minutes</option>
+
+</select>
+
+</div>
+
 <button
-
 class="startBtn"
-
-onclick="location.href='reading.html?id=${test.id}'">
+onclick="startTest('${test.id}')">
 
 Start Test
 
